5347d06e97f0b778b9e81adc10c9046ea4d9f201
Ryo Numoto <ryonumoto@RyonoMacBook-Pro.local>
2020-08-22 14:33:04 +0900

    コメントエリアをコンポーネントに分割

--- pages/anketos/_id.vue
+++ pages/anketos/_id.vue
@@ -12,16 +12,7 @@
       <v-btn @click="executeVote">投票する</v-btn>
     </v-container>
     <hr />
-    <template v-for="(item, index) in items">
-      <v-subheader v-if="item.header" :key="item.header" v-text="item.header"></v-subheader>
-      <v-divider v-else-if="item.divider" :key="index" :inset="item.inset"></v-divider>
-      <v-list-item v-else :key="item.title">
-        <v-list-item-content>
-          <v-list-item-title v-html="item.title"></v-list-item-title>
-          <v-list-item-subtitle v-html="item.subtitle"></v-list-item-subtitle>
-        </v-list-item-content>
-      </v-list-item>
-    </template>
+    <comment-area></comment-area>
     <v-btn
       rounded
       color="primary"
@@ -37,12 +28,14 @@
 
 <script>
 import Chart from "~/components/Chart";
+import CommentArea from "~/components/CommentArea.vue";
 import CreateCommentDialog from "~/components/CreateCommentDialog.vue";
 import axios from "axios";
 
 export default {
   components: {
     Chart,
+    CommentArea,
     CreateCommentDialog,
   },
   async asyncData({ params }) {
@@ -66,38 +59,6 @@ export default {
   data() {
     return {
       selectedOption: null,
-      items: [
-        { header: "コメント一覧" },
-        {
-          title: "Brunch this weekend?",
-          subtitle:
-            "<span class='text--primary'>Ali Connors</span> &mdash; I'll be in your neighborhood doing errands this weekend. Do you want to hang out?",
-        },
-        { divider: true, inset: false },
-        {
-          title: 'Summer BBQ <span class="grey--text text--lighten-1">4</span>',
-          subtitle:
-            "<span class='text--primary'>to Alex, Scott, Jennifer</span> &mdash; Wish I could come, but I'm out of town this weekend.",
-        },
-        { divider: true, inset: false },
-        {
-          title: "Oui oui",
-          subtitle:
-            "<span class='text--primary'>Sandra Adams</span> &mdash; Do you have Paris recommendations? Have you ever been?",
-        },
-        { divider: true, inset: false },
-        {
-          title: "Birthday gift",
-          subtitle:
-            "<span class='text--primary'>Trevor Hansen</span> &mdash; Have any ideas about what we should get Heidi for her birthday?",
-        },
-        { divider: true, inset: false },
-        {
-          title: "Recipe to try",
-          subtitle:
-            "<span class='text--primary'>Britta Holt</span> &mdash; We should eat this: Grate, Squash, Corn, and tomatillo Tacos.",
-        },
-      ],
     };
   },
   methods: {
