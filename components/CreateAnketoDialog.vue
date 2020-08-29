195ab964a1a14b62e20e98b923aeb2a66cd053bf
Ryo Numoto <ryonumoto@RyonoMacBook-Pro.local>
2020-08-29 22:36:23 +0900

    アンケート登録の際にカテゴリを選択できるよう実装

--- components/CreateAnketoDialog.vue
+++ components/CreateAnketoDialog.vue
@@ -14,6 +14,19 @@
             <v-text-field v-model="optionValues[index]" :placeholder="label"></v-text-field>
           </v-col>
         </v-row>
+
+        <v-card-text>
+          <h2 class="title mb-2">カテゴリ選択</h2>
+          <v-chip-group column mandatory>
+            <v-chip
+              filter
+              outlined
+              v-for="category in categories"
+              :key="category.categoryId"
+              @click="selectCategory(category.categoryId)"
+            >{{ category.categoryName }}</v-chip>
+          </v-chip-group>
+        </v-card-text>
       </v-container>
       <v-card-actions>
         <v-spacer></v-spacer>
@@ -33,18 +46,33 @@ export default {
       description: "",
       optionLabels: ["選択肢1", "選択肢2", "選択肢3", "選択肢4"],
       optionValues: ["", "", "", ""],
+      categories: [
+        { categoryName: "恋愛・結婚", categoryId: 1 },
+        { categoryName: "芸能", categoryId: 2 },
+        { categoryName: "仕事", categoryId: 3 },
+        { categoryName: "漫画・アニメ・本", categoryId: 4 },
+        { categoryName: "音楽", categoryId: 5 },
+        { categoryName: "政治・経済", categoryId: 6 },
+        { categoryName: "生活", categoryId: 7 },
+        { categoryName: "スポーツ", categoryId: 8 },
+        { categoryName: "その他", categoryId: 99 },
+      ],
+      categoryId: 99,
     };
   },
   methods: {
     toggle: function () {
       this.createAnketoDialog = !this.createAnketoDialog;
     },
+    selectCategory: function (categoryId) {
+      this.categoryId = categoryId;
+    },
     createAnketo: async function () {
       const payload = {
         title: this.title,
         description: this.description,
         anketoOptions: this.optionValues,
-        categoryId: 99,
+        categoryId: this.categoryId,
       };
       await this.$store.dispatch("createAnketosAction", payload);
       this.toggle();
